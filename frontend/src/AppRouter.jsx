49e3762cf5aea982224320d06162467698cad317
lotus9630 <68753108+lotus9630@users.noreply.github.com>
2022-01-11 15:05:56 +0900

    folder refactoring

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from 'pages/home/HomePage';
-import LoginPage from 'pages/login/LoginPage';
-import SignupPage from 'pages/signup/SingupPage';
+import HomePage from 'pages/home';
+import LoginPage from 'pages/login';
+import SignupPage from 'pages/signup';
 import ErrorPage from 'pages/ErrorPage';
 
 function AppRouter() {
